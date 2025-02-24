c22602fb140d45e4b7c24c4047f3195873ac313f
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-24 10:16:04 +0100

    s3.tf

--- s3.tf
+++ s3.tf
@@ -4,10 +4,14 @@
 
 resource "aws_s3_bucket" "wordpress_bucket" {
   bucket = var.s3_bucket_name
-  acl    = "private"
 
   tags = {
     Name        = "${var.project_name}-wordpress-bucket"
     Environment = var.env
   }
+}
+
+resource "aws_s3_bucket_acl" "wordpress_bucket_acl" {
+  bucket = aws_s3_bucket.wordpress_bucket.id
+  acl    = "private"
 }
