6644c34f42e56393310f158fd03176c140b0568e
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-24 10:00:50 +0100

    ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,41 @@
+##############################
+# EC2 Instance and User Data
+##############################
+
+resource "aws_instance" "instance" {
+  ami                         = var.ami_id
+  instance_type               = "t3.micro"
+  availability_zone           = var.availability_zone_1
+  associate_public_ip_address = true
+  key_name                    = var.key_name
+  subnet_id                   = aws_subnet.public_1.id
+  vpc_security_group_ids      = [aws_security_group.sg_vpc.id]
+  # Update the IAM instance profile if needed:
+  iam_instance_profile        = "your-iam-profile"
+  count                       = 1
+
+  tags = {
+    Name = "${var.project_name}-instance"
+  }
+
+  user_data = base64encode(data.template_file.ec2userdatatemplate.rendered)
+
+  provisioner "local-exec" {
+    command = "echo Instance Type = ${self.instance_type}, Instance ID = ${self.id}, Public IP = ${self.public_ip}, AMI ID = ${self.ami} >> metadata"
+  }
+}
+
+data "template_file" "ec2userdatatemplate" {
+  template = file("userdata.tpl")
+  vars = {
+    bucket_name = var.s3_bucket_name
+  }
+}
+
+output "ec2_rendered_user_data" {
+  value = data.template_file.ec2userdatatemplate.rendered
+}
+
+output "instance_public_ip" {
+  value = aws_instance.instance[0].public_ip
+}
