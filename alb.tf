9433f23f5080054652686a687e42e55729488fcf
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-24 10:01:11 +0100

    alb.tf

---
+++ alb.tf
@@ -0,0 +1,56 @@
+##############################
+# Application Load Balancer (ALB)
+##############################
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "${var.project_name}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = aws_vpc.dev_vpc.id
+
+  tags = {
+    Name = var.project_name
+  }
+
+  health_check {
+    enabled             = true
+    interval            = 10
+    path                = "/"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb" "application_lb" {
+  name               = "${var.project_name}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  security_groups    = [aws_security_group.sg_vpc.id]
+  ip_address_type    = "ipv4"
+
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
+}
+
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.application_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "ec2_attach" {
+  count            = length(aws_instance.instance)
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.instance[count.index].id
+}
