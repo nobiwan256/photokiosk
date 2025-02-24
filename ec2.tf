e5540864d82cd68bd60a5cdfab519e32a25d1032
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-24 12:10:06 +0100

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,12 @@
 # EC2 Instance and User Data
 ##############################
 
+locals {
+  wordpress_userdata = templatefile("${path.module}/userdata.tpl", {
+    wordpress_rds_endpoint = var.wordpress_rds_endpoint
+  })
+}
+
 resource "aws_instance" "instance" {
   ami                         = var.ami_id
   instance_type               = "t2.micro"
@@ -18,24 +24,17 @@ resource "aws_instance" "instance" {
     Name = "${var.project_name}-instance"
   }
 
-  user_data = base64encode(data.template_file.wordpress_userdata.rendered)
+  user_data = base64encode(local.wordpress_userdata)
 
   provisioner "local-exec" {
     command = "echo Instance Type = ${self.instance_type}, Instance ID = ${self.id}, Public IP = ${self.public_ip}, AMI ID = ${self.ami} >> metadata"
   }
 }
 
-data "template_file" "wordpress_userdata" {
-  template = file("userdata.tpl")
-  vars = {
-    wordpress_rds_endpoint = var.wordpress_rds_endpoint
-  }
+output "instance_public_ip" {
+  value = aws_instance.instance[0].public_ip
 }
 
 output "ec2_rendered_user_data" {
-  value = data.template_file.wordpress_userdata.rendered
-}
-
-output "instance_public_ip" {
-  value = aws_instance.instance[0].public_ip
+  value = local.wordpress_userdata
 }
