28519f2d84f5baacb97919393f55231f17bc35d7
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-25 09:50:49 +0100

    providers.tf

--- providers.tf
+++ providers.tf
@@ -18,5 +18,27 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region                  = "us-west-2"
+  access_key              = var.aws_access_key_id     # Use variable placeholders
+  secret_key              = var.aws_secret_access_key
+  token                   = var.aws_session_token
+}
+
+# Define variables
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_session_token" {
+  description = "AWS Session Token"
+  type        = string
+  sensitive   = true
 }
