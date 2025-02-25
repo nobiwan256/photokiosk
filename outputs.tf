412926bb5ebfa5183511694b988f096d06e01649
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-25 09:43:29 +0100

    outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,24 @@
-output "instance_public_ip" {
-  value = aws_instance.instance[0].public_ip
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.dev_vpc.id
 }
 
-output "alb_dns_name" {
-  value = aws_lb.application_lb.dns_name
+output "wordpress_public_ip" {
+  description = "Public IP of the WordPress EC2 instance"
+  value       = aws_instance.wordpress_instance.public_ip
+}
+
+output "wordpress_public_dns" {
+  description = "Public DNS of the WordPress EC2 instance"
+  value       = aws_instance.wordpress_instance.public_dns
+}
+
+output "mysql_endpoint" {
+  description = "Endpoint for the MySQL database"
+  value       = aws_db_instance.wordpress_db.address
+}
+
+output "wordpress_url" {
+  description = "URL to access WordPress"
+  value       = "http://${aws_instance.wordpress_instance.public_dns}"
 }
