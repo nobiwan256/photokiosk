ffa81ea98b36351329012a2b01fcf0e5c30d6029
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-25 09:47:49 +0100

    rds.tf

---
+++ rds.tf
@@ -0,0 +1,32 @@
+##############################
+# RDS MySQL (Free Tier)
+##############################
+
+resource "aws_db_subnet_group" "wordpress_db_subnet_group" {
+  name       = "${var.project_name}-db-subnet-group"
+  subnet_ids = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+
+  tags = {
+    Name = "${var.project_name}-db-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "wordpress_db" {
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "mysql"
+  engine_version         = "5.7"
+  instance_class         = "db.t2.micro"  # Free tier eligible
+  db_name                = var.rds_db_name
+  username               = var.rds_username
+  password               = var.rds_password
+  parameter_group_name   = "default.mysql5.7"
+  db_subnet_group_name   = aws_db_subnet_group.wordpress_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  skip_final_snapshot    = true
+  publicly_accessible    = false
+
+  tags = {
+    Name = "${var.project_name}-wordpress-db"
+  }
+}
