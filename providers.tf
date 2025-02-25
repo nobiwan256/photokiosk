85c000e356f6754a1492b35d8b7d53c2ecacff1e
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-25 10:53:37 +0100

    providers.tf

--- providers.tf
+++ providers.tf
@@ -5,40 +5,8 @@ terraform {
       version = "~> 4.0"
     }
   }
-
-  # If you're using Terraform Cloud, uncomment and modify these lines
-  /*
-  cloud {
-    organization = "your-organization"
-    workspaces {
-      name = "photokiosk-workspace"
-    }
-  }
-  */
 }
 
 provider "aws" {
-  region                  = "us-west-2"
-  access_key              = var.aws_access_key_id     # Use variable placeholders
-  secret_key              = var.aws_secret_access_key
-  token                   = var.token
-}
-
-# Define variables
-variable "aws_access_key_id" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "token" {
-  description = "AWS Session Token"
-  type        = string
-  sensitive   = true
+  region = "us-west-2"
 }
