b7943b8704095b61e48808f6902bb3180ced234b
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-25 14:59:59 +0100

    ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,26 @@
+##############################
+# EC2 Instance
+##############################
+
 resource "aws_instance" "wordpress_instance" {
-  ami                         = var.ami_id
-  instance_type               = "t2.micro"  # Free tier eligible
-  availability_zone           = var.availability_zone_1
-  associate_public_ip_address = true
-  key_name                    = var.key_name
-  subnet_id                   = aws_subnet.public_1.id
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
-  user_data                   = templatefile("userdata.tpl", { db_endpoint = aws_db_instance.wordpress_db.address })
+  ami                    = var.ami_id
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public_1.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = var.key_name
+  user_data              = base64encode(templatefile("userdata.tpl", { db_endpoint = aws_db_instance.wordpress_db.address }))
 
   tags = {
-    Name = "${var.project_name}-wordpress"
+    Name        = "${var.project_name}-wordpress"
+    Environment = var.env
   }
+
+  depends_on = [
+    aws_db_instance.wordpress_db
+  ]
+}
+
+output "wordpress_instance_public_ip" {
+  description = "Public IP address of the WordPress instance"
+  value       = aws_instance.wordpress_instance.public_ip
 }
