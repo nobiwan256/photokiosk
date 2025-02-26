bf3f075b456e527c4f451d0f9396fd224cff81ca
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-26 18:01:57 +0100

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  cloud {
+    organization = "nobiwan"
+    workspaces {
+      name = "photokiosk"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
+  # Credentials will be automatically pulled from environment variables
 }
