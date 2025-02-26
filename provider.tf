dbc8897fee08b4d812c1025860897b48ec6a6fcb
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-26 17:14:54 +0100

    provider.tf

--- provider.tf
+++ provider.tf
@@ -18,5 +18,5 @@ provider "aws" {
   region     = "us-west-2"
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
-  token      = var.token != "" ? var.token : null
+  token      = var.token
 }
