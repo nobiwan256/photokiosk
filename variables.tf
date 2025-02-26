0add17e1da389de5ff0daa7e4aff52da7741eebc
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-26 18:57:18 +0100

    variables.tf

--- variables.tf
+++ variables.tf
@@ -43,7 +43,7 @@ variable "private_subnet_cidr_2" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0df193a2a3a1c90ea"
+  default     = "ami-0747bdcabd34c712a"
 }
 
 variable "key_name" {
