5b64ba2f024e47491ded73a02156620806eee939
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-26 17:14:15 +0100

    variables.tf

--- variables.tf
+++ variables.tf
@@ -44,21 +44,58 @@ variable "aws_access_key_id" {
   description = "AWS Access Key ID"
   type        = string
   sensitive   = true
-  default     = "ASIAXPJ3LA3OSBKBDOPP"
 }
 
 variable "aws_secret_access_key" {
   description = "AWS Secret Access Key"
   type        = string
   sensitive   = true
-  default     = "HD0zCsdW4DRiV6TiUdH37ZdZwDW1dG4CStG9UqUt"
 }
 
-variable "aws_session_token" {
+variable "token" {
   description = "AWS Session Token"
   type        = string
   sensitive   = true
-  default     = <<EOF
-IQoJb3JpZ2luX2VjECYaCXVzLXdlc3QtMiJHMEUCIB73TajBe4UTvvwdmoqei2f7RIl8tpAtvX8FwQHx0NSfAiEAgmch3HugOgkQp47dQR0gdUc0DK92DPqSFaKrfnnov8UqpgIIXxABGgw1MTM5MDk5MTc0MDUiDA04rKPDkS/NJ4MwcyqDAho8xdwXGeJfqCY7yUdRlNc7PY5Yye/qmkzblrhTS7yo9idK8VGgimJACJKmD9dY8SDYhQgikbGqVsgl+f3rhV3dJlz+gPCqcsfZ58RU+VRhfDFvDtMJ2q2FVgY9WUuhmYPtIsAkaI0JGwlNIQgxEWageqxwJ8/U4mdTKNnYuyOn54OhSnwbW7Er6JTPeekgkcN1uRb2eWqT78sE9grBxBt+VrKomcJBV9xAKlU2ZsuPxKpjMSAdNdHBJxpdWP/ROwROj5m3vdkYVUSqv1vLI84Jh7OAFKDUifN3i0qDsKSwzhDDDi+hBRZreOrhXSrcecyvm+nocJA7zG4Ho4L3+9i0wgswlrn8vQY6nQGHQeBhMVRiFIFtEW1MzfKzIfAHNR6ndoYq8dkJ4ZIjVDG81GLGJ2G1DkwtZgMib2ZLX6YHZ7hG/I9UQwsWk96LE9ca9qRkOLsy4RAzyhDB4yJgyMN+qeH64Vgj/D5SuF8Xew2yfP/e4S79ZTr462c0fkdkZGfhiWhvAzjG65lGgTR3dv8LTiRo6Sfx/uIB/Db1ZrakmjZZBrmSUKxF
-EOF
+}
+
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket for WordPress assets"
+  type        = string
+}
+
+variable "rds_password" {
+  description = "Password for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "rds_username" {
+  description = "Username for the RDS instance"
+  type        = string
+  default     = "sriwp_dbuser"
+  sensitive   = true
+}
+
+variable "rds_db_name" {
+  description = "Database name for the RDS instance"
+  type        = string
+  default     = "wordpress_db"
+}
+
+variable "asg_min_size" {
+  description = "Minimum size for the Auto Scaling Group"
+  type        = number
+  default     = 1
+}
+
+variable "asg_max_size" {
+  description = "Maximum size for the Auto Scaling Group"
+  type        = number
+  default     = 2
+}
+
+variable "asg_desired_capacity" {
+  description = "Desired capacity for the Auto Scaling Group"
+  type        = number
+  default     = 1
 }
