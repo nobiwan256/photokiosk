e4a504bdf12d160ae7cfd17e4367d64bea78c6c0
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-26 18:00:40 +0100

    provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-
-  cloud {
-    organization = "nobiwan"
-    workspaces {
-      name = "photokiosk"
-    }
-  }
-}
-
 provider "aws" {
-  region     = "us-west-2"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-  token      = var.token
+  region = "us-west-2"
 }
