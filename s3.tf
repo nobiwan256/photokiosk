4f85cd2d0d0c6f3177eee3eb8a4632e311f59754
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-02-26 18:38:11 +0100

    s3.tf

--- s3.tf
+++ s3.tf
@@ -10,28 +10,3 @@ resource "aws_s3_bucket" "wordpress_bucket" {
     Environment = var.env
   }
 }
-
-resource "aws_s3_bucket_ownership_controls" "wordpress_bucket_ownership" {
-  bucket = aws_s3_bucket.wordpress_bucket.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "wordpress_bucket_public_access" {
-  bucket = aws_s3_bucket.wordpress_bucket.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_versioning" "wordpress_bucket_versioning" {
-  bucket = aws_s3_bucket.wordpress_bucket.id
-  
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
