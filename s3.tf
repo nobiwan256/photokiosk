50075ce328c56e82a1d03c8ff02f63d0f41e8da6
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-03-04 08:34:25 +0100

    s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,3 @@
-# Basic S3 Bucket with minimal permissions
 resource "aws_s3_bucket" "wordpress_bucket" {
   bucket = var.s3_bucket_name
 
@@ -8,29 +7,10 @@ resource "aws_s3_bucket" "wordpress_bucket" {
   }
 }
 
-# Set basic bucket settings
-resource "aws_s3_bucket_ownership_controls" "wordpress_bucket_ownership" {
-  bucket = aws_s3_bucket.wordpress_bucket.id
-  
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-# Set public access block
 resource "aws_s3_bucket_public_access_block" "wordpress_bucket_public_access" {
   bucket                  = aws_s3_bucket.wordpress_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-# Set versioning
-resource "aws_s3_bucket_versioning" "wordpress_bucket_versioning" {
-  bucket = aws_s3_bucket.wordpress_bucket.id
-  
-  versioning_configuration {
-    status = "Disabled"  # Set to Enabled if you need versioning
-  }
 }
