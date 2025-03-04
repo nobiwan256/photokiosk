edcbce917cad811b7f001eab1f8bfa62a948078b
Nobert Latim <69243484+nobiwan256@users.noreply.github.com>
2025-03-04 10:00:28 +0100

    s3.tf

--- s3.tf
+++ s3.tf
@@ -1,16 +1,8 @@
-resource "aws_s3_bucket" "wordpress_bucket" {
-  bucket = var.s3_bucket_name
-
-  tags = {
-    Name        = "${var.project_name}-bucket"
-    Environment = var.env
-  }
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
 }
 
-resource "aws_s3_bucket_public_access_block" "wordpress_bucket_public_access" {
-  bucket                  = aws_s3_bucket.wordpress_bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+resource "aws_s3_bucket" "wordpress_bucket" {
+  bucket = var.s3_bucket_name
 }
